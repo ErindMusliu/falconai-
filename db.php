271c820a9b69d03a82fd0e06b390d2816125c7f8
Erind Musliu <erindmusliu.11@gmail.com>
2026-02-23 15:22:36 +0100

    Refactor database connection using parse_url

--- db.php
+++ db.php
@@ -1,18 +1,22 @@
 <?php
-
 $db_url = getenv('DATABASE_URL');
 
 try {
     if ($db_url) {
-        $pdo = new PDO($db_url);
+        $parts = parse_url($db_url);
+        $dsn = sprintf("pgsql:host=%s;port=%d;dbname=%s", 
+            $parts['host'], 
+            $parts['port'] ?? 5432, 
+            ltrim($parts['path'], '/')
+        );
+        
+        $pdo = new PDO($dsn, $parts['user'], $parts['pass']);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     } else {
-        die("Gabim: Variabla DATABASE_URL nuk u gjet.");
+        die("DATABASE_URL mungon ne Environment Variables!");
     }
 } catch (PDOException $e) {
-    error_log($e->getMessage());
+    error_log("GABIM LIDHJEJE: " . $e->getMessage());
     die("Gabim teknik ne lidhje me databazen.");
 }
-
 ?>
