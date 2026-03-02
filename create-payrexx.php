c2c83e96d52a7608ffe5a69834fd475b3d1f71e1
Erind Musliu <erindmusliu.11@gmail.com>
2026-03-02 15:25:01 +0100

    Update create-payrexx.php

--- create-payrexx.php
+++ create-payrexx.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
 header('Content-Type: application/json');
 
 $input = json_decode(file_get_contents('php://input'), true);
